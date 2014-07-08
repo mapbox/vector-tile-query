c784835a53c144a9ec05c93f2a4c0b461d37b718
rsudekum <bobby@mapbox.com>
2014-07-07 22:19:27 -0700

    update package.json with new mapnik

--- serve.js
+++ serve.js
@@ -5,26 +5,11 @@ var app = express();
 var port = process.env.PORT || 8000;
 var skipValue = 5;
 
-function interpolateBetween(frPoint,toPoint,valueName) {
-    var valueRange = frPoint[valueName]-toPoint[valueName];
-    
-    var idRange = toPoint.id-frPoint.id;
-    var outPoints = [];
-    for (var i=1; i<idRange; i++) {
-        var cID = frPoint.id+i;
-        outPoints.push({
-            value: (1-(i/idRange))*valueRange+toPoint[valueName],
-            id: cID
-        });
-    }
-    return outPoints;
-}
-
 // Elevation query example
 app.get('/query/:source/:layer/:attribute/:format/:data', cors(), function(req, res, next) {
     loadVT(req.params.source, req.params.layer, req.params.attribute, req.params.format, skipValue, req.params.data, function(err, result) {
         if (err) throw err;
-        
+
         res.json(result);
     });
 });
