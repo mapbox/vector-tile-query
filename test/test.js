478e2fd89ab2de98dfea4be3a649b124403a25ce
rsudekum <bobby@mapbox.com>
2014-08-21 12:41:40 -0700

    use new smaller mapnik querymany returns

--- test/test.js
+++ test/test.js
@@ -3,9 +3,9 @@ var fs = require('fs');
 var vtileQuery = require('../');
 
 function readTile(tile,callback) {
-    var tilepath = 'fixtures/tiles/'+tile.z+'/'+tile.x+'/'+tile.y+'.vector.pbf';
+    var tilepath = 'test/fixtures/tiles/'+tile.z+'/'+tile.x+'/'+tile.y+'.vector.pbf';
     fs.readFile(tilepath, function(err,data) {
-        if (err) return callback(err)   
+        if (err) return callback(err)
         return callback(null,data);
     });
 }
@@ -16,7 +16,7 @@ describe('Load relevant tiles from list of coords', function() {
         vtileQuery.loadTiles(queryCoords,14,readTile, function (err,data) {
             assert.equal(err.message,"Tile not loaded")
             done();
-        });   
+        });
     });
 });
 
