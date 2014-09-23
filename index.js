f491284c3a9a0603d5df15c91a394fb53cdbcce6
dnomadb <damon@mapbox.com>
2014-09-23 09:39:32 -0700

    adding ability to fill in null values with linearly interpolated values

--- index.js
+++ index.js
@@ -159,11 +159,19 @@ function queryTile(pbuf, tileInfo, queryPoints, pointIDs, options, callback) {
 
 function multiQuery(dataArr,options,callback) {
 
+    var fillNulls = options.fill !== undefined ? options.fill : false;
+
+
     function queriesDone(err, queries) {
         if (err) return callback(err);
         var dataOutput = [];
         dataOutput = dataOutput.concat.apply(dataOutput, queries);
         dataOutput.sort(sortBy('id'));
+        if (fillNulls) {
+            for (var f = 0; f < options.fields.length; f++) {
+                dataOutput = interpolateResponse(dataOutput, options.fields[f]);
+            }
+        }
         return callback(null, dataOutput);
     }
 
@@ -227,6 +235,73 @@ function convert(queryPoints, pointIDs, fields, interpolate, data) {
         converted.push(res);
     }
     return converted;
+}
+
+function interpolateResponse(inObj, field, callback) {
+
+    function interpolateBetween(fr, to, frID, toID) {
+        var idRange = toID - frID;
+        var outPoints = [];
+        var valueRange = fr - to;
+        for (var i = 1; i < idRange; i++) {
+            outPoints.push((1 - (i / idRange)) * valueRange + to);
+        }
+        return outPoints;
+    }
+
+    function findBreaks(objArr, field) {
+        var uVals = [];
+        var idVals = [];
+        var firstNonNull = 0;
+
+        for (var i = 0; i < objArr.length; i++) {
+            var oObj = null;
+            if (objArr[i][field] !== null && uVals.length === 0) {
+                oObj = objArr[i][field];
+                firstNonNull = i;
+            } else if (objArr[i][field] !== null && objArr[i][field] !== uVals[uVals.length-1]) {
+                oObj = objArr[i][field];
+            } else if (i === objArr.length-1 && objArr[i][field] !== null) {
+                oObj = objArr[i][field];
+            } else if (i === objArr.length-1 && uVals.length !== 0) {
+                oObj = uVals[uVals.length-1];
+            }
+            if (oObj !== null) {
+                uVals.push(oObj);
+                idVals.push(i);
+            }
+        }
+
+        if (firstNonNull !== 0) {
+            idVals.unshift(0);
+            uVals.unshift(uVals[0]);
+        }
+        return {
+            ids: idVals,
+            breaks: uVals
+        };
+    }
+
+    var breaks = findBreaks(inObj, field);
+
+    if (breaks.breaks.length === 0) {
+        return callback(null, inObj);
+    } else if (breaks.breaks.length == inObj.length) {
+        return callback(null, inObj);
+    }
+
+    var output = [breaks.breaks[0]];
+
+    for (var i=1; i< breaks.breaks.length; i++) {
+        output = output.concat(interpolateBetween(breaks.breaks[i-1],breaks.breaks[i],breaks.ids[i-1],breaks.ids[i]));
+        output.push(breaks.breaks[i]);
+    }
+
+    inObj.map(function(record, i) {
+        record[field] = output[i];
+    });
+
+    return callback(null, inObj);
 }
 
 module.exports = {
